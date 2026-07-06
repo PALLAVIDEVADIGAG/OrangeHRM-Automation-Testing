9a06d9a0f804845cb121c1b7dec0ee34d2072782
PALLAVI DEVADIGA <pallu2814@gmail.com>
2026-07-06 14:39:55 +0530

    Update LoginTest.java

--- POM_Framework/Test/LoginTest.java
+++ POM_Framework/Test/LoginTest.java
@@ -9,6 +9,10 @@ import Utilities.DriverFactory;
 public class LoginTest {
 
 	WebDriver driver;
+	LoginPage login;
+	
+	private static final String USERNAME = "Admin";
+	private static final String PASSWORD = "admin123";
 
 	@BeforeMethod
 	public void setup() {
@@ -16,39 +20,74 @@ public class LoginTest {
 		driver = DriverFactory.setup();
 
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		login = new LoginPage(driver);
 
 	}
 
-	// TC-001
-	@Test
-	public void validLogin() {
+	// TC-001 - Verify login with valid credentials
+	@Test (description = "Verify login with valid credentials")
+	public void testvalidLogin() {
 
-		LoginPage login = new LoginPage(driver);
+		
 
-		login.validLogin("Admin", "admin123");
+		login.validLogin(USERNAME, PASSWORD);
 
 		Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
 
 	}
 	
 	@Test
-	public void logout() {
-	    LoginPage login = new LoginPage(driver);
+	public void testlogout() {
+	    
 
-	    login.validLogin("Admin","admin123");
+	    login.validLogin(USERNAME, PASSWORD);
 	    login.logout();
+	    
+	    Assert.assertTrue(login.getCurrenturl().contains("login"));
 	}
 	
 	// TC-002, TC-017
-	@Test(dataProvider = "invalidData")
-	public void invalidLogin(String user, String pwd) {
-		LoginPage login = new LoginPage(driver);
+	@Test(dataProvider = "invalidLoginData")
+	public void testinvalidLogin(String user, String pwd) {
+		
 		login.invalidLogin(user, pwd);
 		String error = login.getErrormsg();
-		Assert.assertTrue(error.contains("Invalid credentials"));
+		Assert.assertEquals(error, "Invalid credentials");
 	}
 	
-	@DataProvider(name = "invalidData")
+
+	
+	@Test
+	public void testemptyFieldValidation() {
+
+	   
+
+	    String message = login.emptyFieldValidation("", "");
+
+	    Assert.assertEquals(message, "Required");
+	}
+	
+	
+	@Test
+	public void testusernameWithSpacesTest() {
+
+	   
+
+	    login.invalidLogin(" Admin ", "admin123");
+
+	    Assert.assertEquals(login.getErrormsg(), "Invalid credentials");
+	}
+	
+	@Test
+	public void testUsernameCaseInsensitivity() {
+
+	    login.validLogin("admin", "admin123");
+
+	    Assert.assertTrue(login.getCurrenturl().contains("dashboard"));
+	}
+	
+	@DataProvider(name = "invalidLoginData")
     public Object[][] invalidData() {
 
         return new Object[][] {
