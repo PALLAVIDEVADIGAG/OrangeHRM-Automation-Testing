e35e9709409c220b9ad2af9c66a2ee283dd7162a
PALLAVI DEVADIGA <pallu2814@gmail.com>
2026-08-12 17:27:01 +0530

    Add files via upload

---
+++ POM_Framework/Base/BaseTest.java
@@ -0,0 +1,30 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import Utilities.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+    	
+    	System.out.println("===== BaseTest.setup() =====");
+
+        driver = DriverFactory.setup();
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        DriverFactory.close();
+    }
+}
